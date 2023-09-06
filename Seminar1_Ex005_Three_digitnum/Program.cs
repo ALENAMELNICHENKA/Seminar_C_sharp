48fd2393b6c0868b456edf5ef92857712a8d0ba5
Alena Melnichenka <6983314@mail.ru>
2023-09-06 14:39:03 +0300

    5

---
+++ Seminar1_Ex005_Three_digitnum/Program.cs
@@ -0,0 +1,15 @@
+﻿// На входе трехзначное число, на выходе последняя цифра введенного числа
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    Console.WriteLine(num % 10);
+}
+
+else
+{
+    Console.Write("Ошибочный ввод данных! ");
+}
+
