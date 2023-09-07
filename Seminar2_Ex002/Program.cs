47c4ac8f2a5c7661b2d703ff59836bda1434e08b
Alena Melnichenka <6983314@mail.ru>
2023-09-07 17:56:36 +0300

    Зад.трехзначное число с удалением второй цифры этого числа

---
+++ Seminar2_Ex002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int num1 = num / 100;
+int num2 = num / 10;
+int num3 = num % 10;
+Console.WriteLine("результат: " + num1 + num3);
+
+
