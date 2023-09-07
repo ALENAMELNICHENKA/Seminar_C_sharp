54308fd0b4767e2b168a4fe83b44cbe60616f512
Alena Melnichenka <6983314@mail.ru>
2023-09-07 19:55:17 +0300

    кратно ли оно

---
+++ Seminar2_Ex004/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num1 % 7 == 0 && num1 % 23 == 0)
+{
+    System.Console.WriteLine("да");
+}
+
+else
+{
+   System.Console.WriteLine("нет"); 
+}
+
