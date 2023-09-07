b88eba22b07b2ab7eef4b76fc0ea426b95931f95
Alena Melnichenka <6983314@mail.ru>
2023-09-07 21:50:59 +0300

    Номер четверти плоскости

---
+++ Seminar3_Ex001/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+int GetNumber(string message)
+{
+    System.Console.WriteLine($"Enter number {message}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void Show(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+    System.Console.WriteLine("1 четверть");
+    }
+
+    else if (x < 0 && y > 0)
+    {
+       System.Console.WriteLine("2 четверть"); 
+    }
+
+    else if (x < 0 && y < 0)
+    {
+       System.Console.WriteLine("3 четверть"); 
+    }
+
+    else if (x > 0 && y < 0)
+    {
+       System.Console.WriteLine("4 четверть"); 
+    }
+
+    else if (x == 0 || y == 0)
+    {
+       System.Console.WriteLine("X ≠ 0 и Y ≠ 0"); 
+    }
+
+}
+int numX = GetNumber("x");
+int numY = GetNumber("x");
+
+Show(numX, numY);
