ec1fdcda23c68a34ce6db4614dd3953fbcb75309
Alena Melnichenka <6983314@mail.ru>
2023-09-26 17:31:39 +0300

    Может ли существовать треугольник со сторонами такой длины

---
+++ Seminar6_Ex002_40/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 40. Напишите программу, которая принимает на вход три числа
+и проверяет, может ли существовать треугольник со сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона меньше суммы двух других сторон.*/
+
+
+Console.WriteLine("Введите три числа: ");
+
+// Метод получения и вывода данных от пользователя
+int GetNumber(string message)
+{
+    System.Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int num1 = GetNumber("Введите число 1 -> ");
+int num2 = GetNumber("Введите число 2 -> ");
+int num3 = GetNumber("Введите число 3 -> ");
+
+if (num1 + num2 > num3 && num2 + num3 > num1 && num1 + num3 > num2)
+{
+    Console.WriteLine("Треугольник существует ");
+}
+else
+{
+    Console.WriteLine("Треугольник не существует ");
+}
+
+
+
+
---
+++ Seminar6_Ex003_45/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 45. Напишите программу, которая будет создавать 
+копию заданного массива с помощью поэлементного копирования.*/
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] array = FillArray(5, 10, 20);
+System.Console.WriteLine();
